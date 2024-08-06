334d9f1c838f556bb0f28472f08cf026cf7fdd8a
leh <kkh60245@gmail.com>
2024-08-06 16:57:10 +0900

    solution-2

--- code77485.py
+++ code77485.py
@@ -1,39 +1,27 @@
-# 3. 행렬을 시계 방향으로 회전시키기
-def rotate(x1, y1, x2, y2, matrix):
-    first = matrix[x1][y1]  # 맨 첫 번째 숫자만 기억해두기
-    min_value = first  # 첫 번째 숫자가 곧 문제의 답이 될 수 있으므로, 따로 저장하기
-
-    # 왼쪽
-    for k in range(x1, x2):
-        matrix[k][y1] = matrix[k + 1][y1]
-        min_value = min(matrix[k][y1], min_value)
-    # 아래
-    for k in range(y1, y2):
-        matrix[x2][k] = matrix[x2][k + 1]
-        min_value = min(matrix[x2][k], min_value)
-    # 오른쪽
-    for k in range(x2, x1, -1):
-        matrix[k][y2] = matrix[k - 1][y2]
-        min_value = min(matrix[k][y2], min_value)
-    # 위
-    for k in range(y2, y1 + 1, -1):
-        matrix[x1][k] = matrix[x1][k - 1]
-        min_value = min(matrix[x1][k], min_value)
-    # 첫 번째 값
-    matrix[x1][y1 + 1] = first
-    return min_value
-
-
 def solution(rows, columns, queries):
     # 1. 행렬 만들기
-    matrix = [
+    board = [
         [(row) * columns + (col + 1) for col in range(columns)] for row in range(rows)
     ]
 
     # 2. 회전해야 할 위치들의 값을 받아오기
     result = []
-    for x1, y1, x2, y2 in queries:
-        result.append(rotate(x1 - 1, y1 - 1, x2 - 1, y2 - 1, matrix))
+    for query in queries:
+        a, b, c, d = query[0] - 1, query[1] - 1, query[2] - 1, query[3] - 1
+        row1, row2 = board[a][b:d], board[c][b + 1 : d + 1]
+        _min = min(row1 + row2)
+
+        for i in range(c, a, -1):
+            board[i][d] = board[i - 1][d]
+            _min = min(_min, board[i][d])
+
+        for i in range(a, c):
+            board[i][b] = board[i + 1][b]
+            _min = min(_min, board[i][b])
+
+        board[a][b + 1 : d + 1], board[c][b:d] = row1, row2
+        result.append(_min)
+
     return result
 
 
