dfc7b61a99b1cbdcb95425030e05c78c11692eb3
leh <kkh60245@gmail.com>
2024-08-08 23:16:32 +0900

    my solution

--- code12930.py
+++ code12930.py
@@ -1,13 +1,17 @@
 def solution(s):
     s = list(s)
+    cnt = 0
     for i in range(len(s)):
         if s[i] == " ":
+            cnt = 0
             continue
-        if i % 2 == 0:
+        if cnt % 2 == 0:
             s[i] = s[i] if s[i].isupper() else s[i].upper()
         else:
             s[i] = s[i].lower if s[i].isupper() else s[i]
+        cnt += 1
         
     return ''.join(s)
 
-print(solution("try hello world"))
+print(solution("try hello worlds"))
+print(solution("give me a cup of water"))
