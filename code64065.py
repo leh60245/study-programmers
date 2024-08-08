86011415c55d7d45fe7af1376023626b634abced
leh <kkh60245@gmail.com>
2024-08-08 23:29:01 +0900

    solution-2

--- code64065.py
+++ code64065.py
@@ -1,12 +1,12 @@
 def solution(s):
-    answer = []
-    data = s[2:-2].split("}, {")
-    data = sorted(data, key=lambda x: len(x))
-    for item in data:
-        item = list(map(int, item.split(",")))
-        for value in item:
-            if value not in answer:
-                answer.append(value)
-    return answer
+    answer = {}
+    s = sorted(s[2:-2].split("}, {"), key=len)
+    for tuples in s:
+        elements = tuples.split(',')
+        for element in elements:
+            number = int(element)
+            if number not in answer:
+                answer[number] = 1
+    return list(answer)
 
 print(solution('{{20, 111}, {111}}'))
