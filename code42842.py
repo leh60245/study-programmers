48d157e22bc2afc15236d1a1119b6f9b14f3f55d
leh <kkh60245@gmail.com>
2024-08-15 16:45:47 +0900

    solution-1

--- code42842.py
+++ code42842.py
@@ -2,12 +2,13 @@ import math
 
 
 def solution(brown, yellow):
-    tmp = int(math.pow(yellow, 1 / 2))
-    for i in range(1, tmp + 1):
-        if yellow % i == 0:
-            share = yellow // i
-            if (share + i) * 2 + 4 == brown:
-                return [share + 2, i + 2]
+    grid = brown + yellow
+    for n in range(3, int(grid**0.5) + 1):
+        if grid % n != 0:
+            continue
+        m = grid // n
+        if (n - 2) * (m - 2) == yellow:
+            return [m, n]
 
 
 print(solution(10, 2))
