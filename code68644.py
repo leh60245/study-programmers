0bd3ac08de466ad32661addda40db39a494aea01
leh <kkh60245@gmail.com>
2024-08-17 18:23:38 +0900

    solution-1.1

--- code68644.py
+++ code68644.py
@@ -2,12 +2,7 @@ from itertools import combinations
 
 
 def solution(numbers: list):
-    arr = set()  # 숫자 중복을 방지하기 위한 set 지정
-    selects = list(combinations(numbers, 2))  # 순열로 2개 선택
-    for select in selects:
-        (a, b) = select
-        arr.add(a + b)
-    return sorted(arr)
+    return sorted(set(map(sum, combinations(numbers, 2))))
 
 
 print(solution([2, 1, 3, 4, 1]))
