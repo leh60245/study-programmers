f8500a00699e83168d1b5a5782cd95fa2402ca1b
leh <kkh60245@gmail.com>
2024-08-17 18:22:47 +0900

    solution-1

--- code68644.py
+++ code68644.py
@@ -1,8 +1,13 @@
-def solution(numbers: list):
-    arr = set() # 숫자 중복을 방지하기 위한 set 지정
-    for i in range(len(numbers)-1):
-        for j in range(i+1, len(numbers)):
-            arr.add(numbers[i] + numbers[j])
-    return sorted(list(arr))    # list로 변경 후 오름차순 나열
+from itertools import combinations
 
-print(solution([2,1,3,4,1]))
+
+def solution(numbers: list):
+    arr = set()  # 숫자 중복을 방지하기 위한 set 지정
+    selects = list(combinations(numbers, 2))  # 순열로 2개 선택
+    for select in selects:
+        (a, b) = select
+        arr.add(a + b)
+    return sorted(arr)
+
+
+print(solution([2, 1, 3, 4, 1]))
