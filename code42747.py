09f7e2336d6befe7934b5ba31768b3ed0ea14077
leh <kkh60245@gmail.com>
2024-08-18 17:58:11 +0900

    solution-1

---
+++ code42747.py
@@ -0,0 +1,11 @@
+def solution(citations):
+    citations.sort()
+    for i in range(len(citations)):
+        if citations[i] >= len(citations) - i:
+            return len(citations) - i
+        
+    return 0
+
+
+citations = [3, 0, 6, 1, 5]
+print(solution(citations))
