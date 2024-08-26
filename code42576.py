302219b4b9f49340de09d6905ce1952fc195e7ba
leh <kkh60245@gmail.com>
2024-08-26 21:31:19 +0900

    solution-1

---
+++ code42576.py
@@ -0,0 +1,13 @@
+def solution(participant, completion):
+    answer = {}
+    for i in participant:
+        answer[i] = answer.get(i, 0) + 1
+    for j in completion:
+        answer[j] -= 1
+    for k in answer:
+        if answer[k]:
+            return k
+        
+participant = ["leo", "kiki", "eden"]
+completion = ["eden", "kiki"]
+print(solution(participant, completion))
