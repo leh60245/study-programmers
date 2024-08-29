89318eb647a9e08cd2e07c1196e362821b2e2640
leh <kkh60245@gmail.com>
2024-08-29 23:37:38 +0900

    부분 회전

---
+++ 삼성/기본 알고리즘/partial_rotation.py
@@ -0,0 +1,24 @@
+
+def rotate_90(sy, sx, length):
+    global arr, new_arr
+    for y in range(sy, sy + length):
+        for x in range(sx, sx + length):
+            oy, ox = y - sy, x - sx
+            ry, rx = ox, length - oy - 1
+            new_arr[sy + ry][sx + rx] = arr[y][x]
+
+    for y in range(sy, sy + length):
+        for x in range(sx, sx + length):
+            arr[y][x] = new_arr[y][x]
+            print(arr[y][x])
+
+
+arr = [[7 * j + i for i in range(1, 8)] for j in range(7)]
+new_arr = [[0] * 7 for _ in range(7)]
+sy, sx = 2, 2
+length = 3
+
+rotate_90(sy, sx, length)
+
+for i in range(len(arr)):
+    print(arr[i])
